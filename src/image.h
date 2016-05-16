c7f93c89587c5bad5f62ada06c98b0d0a318f235
Andrew Vy <andrew@andrewvy.com>
2016-05-16 10:29:52 -0700

    :sparkles: CMake support.

--- src/image.h
+++ src/image.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <GLFW/glfw3.h>
-#include "third-party/lodepng.h"
+#include "lodepng.h"
 
 GLuint loadBMPImage(const char *image);
 GLuint loadPNGImage(const char *file_name);
